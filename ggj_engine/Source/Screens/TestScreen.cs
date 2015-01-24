245d300af815154be67197ba50eb653ace508140
skydereign <lsdawson@ucsc.edu>
2015-01-23 20:56:17 -0800

    Add base TileGrid

--- ggj_engine/Source/Screens/TestScreen.cs
+++ ggj_engine/Source/Screens/TestScreen.cs
@@ -1,4 +1,5 @@
 ﻿using ggj_engine.Source.Entities;
+using ggj_engine.Source.Level;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,13 @@ namespace ggj_engine.Source.Screens
 {
     public class TestScreen : Screen
     {
+        public TileGrid level;
+
         public TestScreen()
         {
             AddEntity(new TestEntity(new Vector2(100, 100)));
+
+            level = new TileGrid(10, 10, new Vector2(0, 0));
         }
 
         public override void Update(GameTime gameTime)
