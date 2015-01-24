feeb140e2c29610b00aa84507216de8b6624d888
Andrew Guerra <anchguerra@gmail.com>
2015-01-23 19:35:37 -0800

    Begin base implementation of Enemy abstract class.

---
+++ ggj_engine/Source/Entities/Enemies/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ggj_engine.Source.Entities.Enemies
+{
+    public abstract class Enemy : Entity
+    {
+        protected int Health;
+        protected int Damage;
+        protected float Speed;
+        
+        protected virtual void SetDecisionTree()
+        {
+            //
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            sprite.Position = Position;
+            if(Health < 0)
+            {
+                Destroy();
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
+
+        protected void UpdateHealth()
+        {
+            Health--;
+        }
+    }
+}
