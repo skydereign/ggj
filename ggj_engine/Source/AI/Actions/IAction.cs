11c4d8843fe5956e03c451905fcc5628474c05c3
Andrew Guerra <anchguerra@gmail.com>
2015-01-23 19:23:02 -0800

    Decision Tree architecture implemented.

---
+++ ggj_engine/Source/AI/Actions/IAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ggj_engine.Source.AI.Actions
+{
+    public interface IAction
+    {
+        void DoAction();
+    }
+}
