7fcd1d162b3f2f0f686acff178a072ebb4e54b37
anchgu7 <soccerplayer7ag@msn.com>
2015-01-25 01:41:51 -0800

    Begin work on YourMom behavior. Running into CurrentPath being null when trying to follow the player.

--- ggj_engine/Source/AI/Actions/FollowPlayerAction.cs
+++ ggj_engine/Source/AI/Actions/FollowPlayerAction.cs
@@ -1,6 +1,9 @@
 ﻿using ggj_engine.Source.AI.DecisionTree;
 using ggj_engine.Source.Entities.Enemies;
+using ggj_engine.Source.Entities.Player;
+using ggj_engine.Source.Level;
 using ggj_engine.Source.Screens;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,7 +31,20 @@ namespace ggj_engine.Source.AI.Actions
         {
             if(enemy.CurrentPath.Count <= 0)
             {
-                enemy.CurrentPath = Pathing.Pathing.FindPath(enemy, enemy.MyScreen.GetEntity("Player").ElementAt(0).Position);
+                Player player = (Player)enemy.MyScreen.GetEntity("Player").ElementAt(0);
+                if(enemy.GetType().Name.Equals("YourMom"))
+                {
+                    float angle = (float)Math.Atan2(player.Position.Y / 16 - enemy.Position.Y / 16, player.Position.X / 16 - enemy.Position.X / 16);
+                    Vector2 target = new Vector2((float)(player.Position.X / 16 - Math.Cos(angle) * 2.0f),
+                        (float)(player.Position.Y / 16 + Math.Sin(angle) * 2.0f));
+
+                    enemy.CurrentPath = Pathing.Pathing.FindPath(enemy, target);
+
+                }
+                else
+                {
+                    enemy.CurrentPath = Pathing.Pathing.FindPath(enemy, player.Position);
+                }
                 enemy.CurrentTile = enemy.CurrentPath.Pop();
                 enemy.PopOffTop = false;
             }
