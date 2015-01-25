27c16739cd285846c09f28667a7ebef8c5106afb
skydereign <lsdawson@ucsc.edu>
2015-01-25 06:08:35 -0800

    Add tile collisions

--- ggj_engine/Source/Utility/Math.cs
+++ ggj_engine/Source/Utility/Math.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,12 @@ namespace ggj_engine.Source.Utility
             double mult = Math.Pow(10.0, digits);
             double result = Math.Truncate(mult * value) / mult;
             return (float)result;
+        }
+
+        public static float Direction(Vector2 from, Vector2 to)
+        {
+            //return (float)Math.Atan2(to.Y - from.Y, from.X - to.X);
+            return (float)Math.Atan2(from.Y - to.Y, to.X - from.X);
         }
     }
 }
